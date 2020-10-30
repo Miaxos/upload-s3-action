fd07cc1ff4ff40e96ca7d56bd6ee20e4a66143ef
Anthony Griffon <anthony.griffon@iadvize.com>
2020-10-30 01:53:43 +0100

    wip

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function upload(params) {
   return new Promise(resolve => {
     s3.upload(params, (err, data) => {
       if (err) core.error(err);
-      core.info(`uploaded - ${data}`);
+      core.info(`uploaded - ${data.Key}`);
       core.info(`located - ${data.Location}`);
       resolve(data.Location);
     });
